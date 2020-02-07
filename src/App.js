728235757cfd339d2a1ff4ac0d750d8b954b7857
Eugene <inhuman.en@gmail.com>
2020-02-07 17:35:00 +0200

    interim commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Grid, Column, Toolbar, Button, PivotGrid } from "@sencha/ext-react-modern";
 import data from './mockdata';
-
+import ReactDOMServer from 'react-dom/server';
 Ext.require("Ext.grid.plugin.Summary");
 Ext.require("Ext.grid.plugin.RowExpander");
 Ext.require("Ext.data.summary.*");
@@ -19,25 +19,26 @@ const createStore = () => Ext.create('Ext.data.Store', {
   // data: data.slice(0, 3),
 });
 
-const gridColumns = [
+const firstColumns = [
   {
     text: "JSX cell",
     dataIndex: "name",
     key: "name",
     width: 150,
-    renderer: renderLink,
-    cell: { xtype: 'reactcell',  encodeHtml: false, },
-    summaryRenderer: v => <button type="button">jsx in summary row</button>
+    renderer: v => v + ' hello'
+    // renderer: renderLink,
+    // cell: { xtype: 'reactcell',  encodeHtml: false, },
+    // summaryRenderer: v => <button type="button">jsx in summary row</button>
   },
   {
-    text: "some number",
+    text: 'title',
     dataIndex: "num",
     key: "num",
     width: 200,
     cell: { xtype: 'reactcell',  encodeHtml: false, },
     renderer: v => <strong>{v}</strong>,
     summary: 'sum',
-    summaryRenderer: v => `$${v}`
+    // summaryRenderer: v => `$${v}`
   },
   {
     text: "email",
@@ -45,7 +46,7 @@ const gridColumns = [
     key: "email",
     width: 200,
     summary: 'count',
-    summaryRenderer: v => `Count - ${v}`
+    // summaryRenderer: v => `Count - ${v}`
   },
   {
     text: 'Column group',
@@ -67,6 +68,30 @@ const gridColumns = [
     ]
   }
 ];
+
+const secondColumns = [
+  {
+    text: "Another number column",
+    dataIndex: "num",
+    key: "second-num",
+    width: 300,
+    // cell: { xtype: 'reactcell',  encodeHtml: false, },
+    // renderer: v => <em>{v}</em>,
+    summary: 'average',
+    // summaryRenderer: v => `$${v}`
+    renderer: v => v + ' bye'
+  },
+  {
+    text: "Another email",
+    dataIndex: "email",
+    key: "second-email",
+    width: 300,
+    summary: 'count',
+    cell: { xtype: 'reactcell',  encodeHtml: false, },
+    renderer: v => <em>{v}</em>,
+    // summaryRenderer: v => `Total - ${v}`
+  },
+]
 
 const virtualGridColumns = [
   {
@@ -122,12 +147,12 @@ const grouperFn = function (rec) {
   return `${rec.get('name')} -  ${rec.get('email')}`;
 };
 const jsxGrouperFn = function (rec) {
-  return <strong>{rec.get('name')} {rec.get('email')}</strong>;
+  return ReactDOMServer.renderToStaticMarkup(<strong>{rec.get('name')} {rec.get('email')}</strong>);
 };
 
 const labelRenderer= v => v + ' group';
-const jsxLabelRenderer= v => <strong>{v}</strong>;
-     
+const jsxLabelRenderer= v => ReactDOMServer.renderToStaticMarkup(<strong>{v}</strong>);
+
 const pivotMatrix = {
   type: 'local',
   collapsibleRows: true,
@@ -140,12 +165,18 @@ const pivotMatrix = {
     dataIndex: 'email',
     width: 200,
     grouperFn: jsxGrouperFn,
+    // column: {
+    //   cell: {
+    //     xtype: 'reactcell', encodeHtml: false,
+    //   }
+    // }
     // labelRenderer: jsxLabelRenderer
   },
   {
     header: 'Second',
     dataIndex: 'name',
     width: 200,
+    labelRenderer: jsxLabelRenderer
   }],
   aggregate: [{
     header: 'Count',
@@ -169,12 +200,20 @@ class App extends Component {
     this.state = {
       showGrid: true,
       bordered: false,
+      switchColumns: false,
+      switchStore: false,
+      gridData: [...data],
     };
 
     this.changeState = this.changeState.bind(this);
     this.changeStyle = this.changeStyle.bind(this);
-    this.destroyGrid = this.destroyGrid.bind(this);
+    this.switchGridColumns = this.switchGridColumns.bind(this);
+    this.switchGridStore = this.switchGridStore.bind(this);
+    this.setNewData = this.setNewData.bind(this);
+    this.updateDataInState = this.updateDataInState.bind(this);
   }
+
+  testPluginFn () {}
 
   changeState() {
     this.setState({
@@ -188,15 +227,48 @@ class App extends Component {
     });
   }
 
-  destroyGrid() {
+  switchGridColumns() {
+    // this.setState(state => ({
+    //   switchColumns: !state.switchColumns,
+    // }));
+
+    this.grid.cmp.setColumns([])
+      .setColumns(secondColumns)
+      .setStore(this.grid.cmp.getStore());
+  }
+
+  switchGridStore() {
+    this.setState(state => ({
+      switchStore: !state.switchStore,
+    }));
+  }
+
+  updateDataInState() {
     this.setState({
-      showGrid: false
+      gridData: [
+        {name: 'Eugene', email: 'eugene@gmail.com', num: Math.random()},
+        {name: 'Eugene', email: 'eugene@gmail.com', num: Math.random()},
+        {name: 'Eugene', email: 'eugene@gmail.com', num: Math.random()},
+        {name: 'Eugene', email: 'eugene@gmail.com', num: Math.random()},
+        {name: 'Eugene', email: 'eugene@gmail.com', num: Math.random()},
+        {name: 'Eugene', email: 'eugene@gmail.com', num: Math.random()},
+        {name: 'Eugene', email: 'eugene@gmail.com', num: Math.random()},
+      ]
     });
+  }
+
+  setNewData() {
+    this.grid.cmp.getStore().setData([
+      {name: 'Helen', email: 'helen@gmail.com', num: Math.random()},
+      {name: 'Helen', email: 'helen@gmail.com', num: Math.random()},
+      {name: 'Helen', email: 'helen@gmail.com', num: Math.random()},
+      {name: 'Helen', email: 'helen@gmail.com', num: Math.random()},
+    ]);
   }
 
   rowTpl (record) {
     console.log('rowTpl');
-    return <div style={{ height: '20px', background: 'lightblue' }}>{record.email}</div>
+    return <div style={{ height: '20px', background: 'lightblue' }}>{record.email}</div>;
   }
 
   toggleRow (grid, target) {
@@ -205,8 +277,22 @@ class App extends Component {
     target.record.set('email', 'changed');
   }
 
+  // componentDidUpdate(prevProps, prevState) {
+  //   console.log('update');
+  //   this.setNewData();
+  // }
+
   render() {
     console.log('render');
+    const { gridData } = this.state;
+
+    const columnsSet = firstColumns;
+
+    const gridStore = {
+        xtype: 'store',
+        proxy: { type: 'memory' },
+        data
+    };
 
     return (
       <div>
@@ -217,25 +303,27 @@ class App extends Component {
         maxHeight="100vh"
       >
         <Toolbar docked="top">
-          <Button text="Change State" handler={this.changeState}/>
+          <Button id="testButtonId" text="Change State" handler={this.changeState}/>
           <Button text="Change Style" handler={this.changeStyle}/>
-          <Button text="Destroy Grid" handler={this.destroyGrid}/>
+          <Button text="Switch columns" handler={this.switchGridColumns}/>
+          <Button text="Switch store" handler={this.switchGridStore}/>
+          <Button text="Set new data" handler={this.setNewData}/>
         </Toolbar>
-          <PivotGrid
+          {/* <PivotGrid
             title="Pivot Grid"
             matrix={pivotMatrix}
-            height={300}
-          />
-          {this.state.showGrid && <Grid
+            height={350}
+          /> */}
+          <Grid
+            ref={grid => {
+              this.grid = grid;
+            }}
             title="Grid"
-            height={300}
+            height={350}
             weighted
             extname="grid1"
-            store={{
-              proxy: { type: 'memory' },
-              data
-            }}
-            columns={gridColumns}
+            store={gridStore}
+            columns={columnsSet}
             infinite={false}
             style={{ border: this.state.bordered ? '2px solid blue' : '0px solid blue' }}
             plugins={{
@@ -245,24 +333,25 @@ class App extends Component {
                   weight: 1,
                 },
               },
-              rowexpander: {
-                column: {
-                  width: 35,
-                },
-              },
+              // rowexpander: {
+              //   column: {
+              //     width: 35,
+              //   },
+              // },
               gridfilters: true,
             }}
-            itemConfig={{
-              body: {
-                tpl: this.rowTpl,
-              },
-            }}
+            // itemConfig={{
+            //   body: {
+            //     tpl: this.rowTpl,
+            //   },
+            // }}
             listeners={{
               childtap: this.toggleRow
             }}
             variableHeights
-          />}
-          <Grid
+          />
+
+          {/* <Grid
             height={400}
             weighted
             store={{
@@ -307,7 +396,7 @@ class App extends Component {
               childtap: this.toggleRow
             }}
             variableHeights
-          />
+          /> */}
       </Container>
       </div>
     )
